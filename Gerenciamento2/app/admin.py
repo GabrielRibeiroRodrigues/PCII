e9e201e946a9d97d983cdb968c6e1722a787841e
Gabriel Ribeiro Rodrigues <103051424+GabrielRibeiroRodrigues@users.noreply.github.com>
2024-07-14 23:16:56 -0300

    Gerenciamento

---
+++ Gerenciamento2/app/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(Produto)
+admin.site.register(DetalheProduto)
+admin.site.register(TipoEmbalagem)
+admin.site.register(Modelo)
+admin.site.register(Fabricante)
+admin.site.register(Marca)
+admin.site.register(Instituic)
+admin.site.register(InstituicUnidade)
+admin.site.register(SectorSuper)
+admin.site.register(Sector)
+admin.site.register(Subsector)
+admin.site.register(Categoria)
+admin.site.register(SubCategoria)
+admin.site.register(Transaction)
+admin.site.register(UnidadeMedida)
+admin.site.register(Tipo)
+admin.site.register(Subtipo)
+admin.site.register(MovimentacaoProduto)
+
+
