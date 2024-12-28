f7a4d1661a624b4c4812d4af8a2de69ce55fc6da
GabrielRibeiroRodrigues <gabriel.ribeiro@alunos.ifsuldeminas.edu.br>
2024-12-28 18:07:44 -0300

    Inlines finalizados

--- Gerenciamento 11/app/admin.py
+++ Gerenciamento 11/app/admin.py
@@ -7,9 +7,13 @@ from .models import CustomUser
 class DetalheProdutoEstoqueInline(admin.TabularInline):
     model = DetalheProdutoEstoque
     extra = 1
+class DetalheProdutoFotoInline(admin.TabularInline):
+    model = DetalheProdutoFoto
+    extra = 1
+admin.site.register(DetalheProdutoFoto)
 
 class DetalheProdutoAdmin(admin.ModelAdmin):
-    inlines = [DetalheProdutoEstoqueInline]
+    inlines = [DetalheProdutoEstoqueInline, DetalheProdutoFotoInline]
     
 admin.site.register(DetalheProduto, DetalheProdutoAdmin)
 admin.site.register(DetalheProdutoEstoque)
@@ -35,21 +39,51 @@ admin.site.register(Instituic, InstituicAdmin)
 admin.site.register(InstituicUnidade)
 
 
+class SubCategoriaUInline(admin.TabularInline):
+    model = SubCategoria
+    extra = 1
+
+class CategoriaAdmin(admin.ModelAdmin):
+    inlines = [SubCategoriaUInline]
+    
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(SubCategoria)
+
+class SubTipoUInline(admin.TabularInline):
+    model = Subtipo
+    extra = 1
+
+class TipoAdmin(admin.ModelAdmin):
+    inlines = [SubTipoUInline]
+ 
+    
+admin.site.register(Tipo, TipoAdmin)
+admin.site.register(Subtipo)
+
+
+class SectorUInline(admin.TabularInline):
+    model = Sector
+    extra = 1
+class SuperSectorAdmin(admin.ModelAdmin):
+    inlines = [SectorUInline]
+
+admin.site.register(SectorSuper, SuperSectorAdmin)
+
+
+
+
 admin.site.register(Produto)
 admin.site.register(TipoEmbalagem)
 admin.site.register(Modelo)
 admin.site.register(Fabricante)
 admin.site.register(Marca)
-admin.site.register(SectorSuper)
-admin.site.register(Categoria)
-admin.site.register(SubCategoria)
 admin.site.register(Transaction)
 admin.site.register(UnidadeMedida)
-admin.site.register(Tipo)
-admin.site.register(Subtipo)
 admin.site.register(MovimentacaoProduto)
 admin.site.register(ProdutoMovimentoItem)
-admin.site.register(DetalheProdutoFoto)
+
+
+
 class CustomUserAdmin(BaseUserAdmin):
     model = CustomUser
     list_display = ('username', 'email', 'subsetor', 'is_staff')
